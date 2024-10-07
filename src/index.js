8283b3c47b734f8595fa40f857011bcade6b0421
Josué Quintana. S <160699958+KingtanaSr@users.noreply.github.com>
2024-10-06 22:22:45 -0600

    Update index.js
    Comentarios para entendimiento xd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const express = require('express');
+// Express es un framework que facilita la definición de rutas 
+//para manejar diferentes solicitudes HTTP (GET, POST, PUT, DELETE, etc)
+
+const express = require('express');  
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
@@ -7,7 +10,7 @@ const publicationRoutes = require('./routes/tasks.routes');
 app.listen(3000)
 app.use(cors()); 
 app.use(morgan('dev'))
-app.use(express.json())
+app.use(express.json()) // Permite al servidor manejar correctamente los datos en formato JSON enviados en el cuerpo de las solicitudes HTTP
 
 app.use(publicationRoutes);
 
